72371a18019b27ff0a6d7628f0d7887031b3c6fc
Sebastian Silva <sebastian@fuentelibre.org>
2017-01-06 13:57:16 -0500

    Comment out updated_at in drupal_node.rb

--- app/models/drupal_node.rb
+++ app/models/drupal_node.rb
@@ -22,10 +22,10 @@ class DrupalNode < ActiveRecord::Base
 
   searchable do
     text :title, boost: 5
-    text :body do
+    string :body do
       body.to_s.gsub!(/[[:cntrl:]]/,'')
     end
-    time :updated_at
+    #time :updated_at
     string :status
     string :updated_month
     text :comments do

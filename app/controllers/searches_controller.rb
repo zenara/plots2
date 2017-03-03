3c9b4cd24897384a898b6b5f15ac4644ba3be39d
jywarren <jeff@unterbahn.com>
2017-03-03 14:24:32 -0500

    search results test tweak

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -14,7 +14,7 @@ class SearchesController < ApplicationController
     @search = DrupalNode.search do
       fulltext term
     end
-    render json: @search[0]
+    render json: @search.results[0]
   end
 
   def new
